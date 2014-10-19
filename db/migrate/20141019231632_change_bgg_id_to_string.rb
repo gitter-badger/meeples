5f73b94af50144d95a213860778eeb3b082642a6
Chavez <matthew@el-chavez.me>
2014-10-19 16:18:28 -0700

    Change bgg_id on games to a string

---
+++ db/migrate/20141019231632_change_bgg_id_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeBggIdToString < ActiveRecord::Migration
+  def change
+    change_column :games, :bgg_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019205350) do
+ActiveRecord::Schema.define(version: 20141019231632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 20141019205350) do
   add_index "game_players", ["player_id"], name: "index_game_players_on_player_id", using: :btree
 
   create_table "games", force: true do |t|
-    t.integer  "bgg_id"
+    t.string   "bgg_id"
     t.integer  "year_published"
     t.string   "name",           null: false
     t.datetime "created_at"
