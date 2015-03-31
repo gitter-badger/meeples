42c08a701d24e90040a06762bd1476d85ddcdf25
Chavez <matthew@el-chavez.me>
2015-03-30 19:43:29 -0700

    Docker setup

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-redis_url = Rails.env.staging? || Rails.env.production? ? ENV['REDISCLOUD_URL'] : 'redis://127.0.0.1'
+redis_url = if Rails.env.staging? || Rails.env.production?
+  ENV['REDISCLOUD_URL']
+else
+  "redis://#{ENV.fetch('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')}:#{ENV.fetch('REDIS_PORT_6379_TCP_PORT', 6379)}"
+end
 Redis.current = Redis.new(url: redis_url) if redis_url
